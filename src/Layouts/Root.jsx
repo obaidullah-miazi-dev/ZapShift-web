fcddd9b2aa1a0fc11d1dcde6c9e2fa9b2a45e8f3
obaidullah-miazi-dev <obaidullahmiazi.dev@gmail.com>
2025-11-15 20:31:33 +0600

    project started

---
+++ src/Layouts/Root.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Navbar from '../components/Navbar';
+
+const Root = () => {
+    return (
+        <div className='bg-[#eaeced] min-h-screen py-5'>
+            <Navbar></Navbar>
+        </div>
+    );
+};
+
+export default Root;
